336aa2294142cdbf6803e3e70e27f3cbf4afe7ed
Tamara Bonyai <tamamara18@gmail.com>
2020-01-30 21:51:51 +0100

    Quest React 2 chat-app 1.0 finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact 
+        nameavatar= 'https://randomuser.me/api/portraits/women/77.jpg'
+        name= 'Beth Collins'
+        online
+      />
+      <Contact 
+        nameavatar= 'https://randomuser.me/api/portraits/men/74.jpg'
+        name= 'Marvin Anderson'
+      />
+      <Contact 
+        nameavatar= 'https://randomuser.me/api/portraits/women/67.jpg'
+        name= 'Leona Knight'
+        online
+      />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Contact.css';
+
+
+function Contact(props) {
+        
+    return (
+        <div className='Contact'>
+            <img className='avatar' src={props.nameavatar} alt={props.name}/>
+            <div className='status'>
+                <h4 className='name'> {props.name} </h4>  
+                <p className='status-text'> 
+                    <span className={props.online === true ? 'status-online' : 'status-offline'}></span> 
+                    {props.online === true ? 'Online' : 'Offline'} 
+                </p>
+            </div>
+        </div>
+    );
+}
+
+
+export default Contact;
