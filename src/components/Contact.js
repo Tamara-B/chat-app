695b95c31a8ea6eb10ea8f58f179e22bacd8caca
Tamara Bonyai <tamamara18@gmail.com>
2020-02-03 18:31:43 +0100

    Onclick status modification 1.0

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,33 @@ import React from 'react';
 import './Contact.css';
 
 
-function Contact(props) {
-        
-    return (
-        <div className='Contact'>
-            <img className='avatar' src={props.avatar} alt={props.name}/>
-            <div className='status'>
-                <h4 className='name'> {props.name} </h4>  
-                <p className='status-text'> 
-                    <span className={props.online === true ? 'status-online' : 'status-offline'}></span> 
-                    {props.online === true ? 'Online' : 'Offline'} 
-                </p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          online: props.online
+        };
+      }
+    
+    render() {
+        return (
+            <div className='Contact'>
+                <img className='avatar' src={this.props.avatar} alt={this.props.name}/>
+                <div className='status'>
+                    <h4 className='name'> {this.props.name} </h4>  
+                    <p className='status-text'> 
+                        <span className={this.state.online ? 'status-online' : 'status-offline'} 
+                        onClick={event => {
+                            const newStatus = !this.state.online;
+                            this.setState({ online: newStatus });
+                        }} > 
+                        <span className='insideText'>{this.state.online ? 'Online' : 'Offline'} </span>
+                        </span>
+                    </p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 
